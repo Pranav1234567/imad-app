ab061c5fae495a19153ce1098eb4ca2b33255e99
Pranav1234567 <pnpiano@gmail.com>
2017-08-11 18:22:01 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,29 +31,25 @@ submit.onclick = function () {
     // Make the request
     var request = new XMLHttpRequest();
     
-    // Capture list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML=list;
-    
     // Capture the response and store it in a variable
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE){
             if (request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                // Capture list of names and render it as a list
+                var names = ['name1', 'name2', 'name3', 'name4'];
+                var list = '';
+                for (var i=0; i<names.length;i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML=list;
             }
         }
         // Not done yet
     };
     
     // Make the request
-    request.open('GET','http://pnpiano.imad.hasura-app.io/counter', true);
+    request.open('GET','http://pnpiano.imad.hasura-app.io/submit-name', true);
     request.send(null);
 };
 
