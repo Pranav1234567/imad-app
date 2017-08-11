b9e09f0411e63859e90956fc267f89c1446ac7c0
Pranav1234567 <pnpiano@gmail.com>
2017-08-11 18:32:59 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,8 @@ button.onclick = function () {
 
 // Submit Name page code
 var submit = document.getElementById('submit-btn');
-
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 
 submit.onclick = function () {
     // Make the request
@@ -47,9 +48,6 @@ submit.onclick = function () {
         }
         // Not done yet
     };
-    
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
     
     // Make the request
     request.open('GET','http://pnpiano.imad.hasura-app.io/submit-name?name=' + name, true);
